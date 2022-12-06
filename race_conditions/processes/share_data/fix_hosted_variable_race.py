8e0acf426c273bd487e488b50f6f14ab55ef7b6d
mikeyy2 <adhikari.shrish1@gmail.com>
2022-12-06 11:14:11 +0545

    Fixed typo and bug in fix_hosted_variable_race.py

--- race_conditions/processes/share_data/fix_hosted_variable_race.py
+++ race_conditions/processes/share_data/fix_hosted_variable_race.py
@@ -5,12 +5,13 @@ from multiprocessing.managers import BaseManager
 from multiprocessing import Lock
 
 
-class UnsafeCounter:
+class SafeCounter:
     # custom counter class that encourages the os to context switch
     # in the middle of the operations, essentially forcing a
     # race condition
     def __init__(self, count):
         self._value = count
+        self._lock = Lock()
 
     # retrieve the variable
     def get(self):
@@ -18,29 +19,31 @@ class UnsafeCounter:
 
     # add one to the variable
     def increment(self):
-        # copy value
-        tmp = self._value
-        # allow a context switch
-        sleep(0)
-        # increment the value
-        tmp = tmp + 1
-        # allow a context switch
-        sleep(0)
-        # copy the updated value
-        self._value = tmp
+        with self._lock:
+            # copy value
+            tmp = self._value
+            # allow a context switch
+            sleep(0)
+            # increment the value
+            tmp = tmp + 1
+            # allow a context switch
+            sleep(0)
+            # copy the updated value
+            self._value = tmp
 
     # subtract one from the variable
     def decrement(self):
-        # copy value
-        tmp = self._value
-        # allow a context switch
-        sleep(0)
-        # increment the value
-        tmp = tmp - 1
-        # allow a context switch
-        sleep(0)
-        # copy the updated value
-        self._value = tmp
+        with self._lock:
+            # copy value
+            tmp = self._value
+            # allow a context switch
+            sleep(0)
+            # increment the value
+            tmp = tmp - 1
+            # allow a context switch
+            sleep(0)
+            # copy the updated value
+            self._value = tmp
 
 
 # custom manager to support custom classes
@@ -50,37 +53,32 @@ class CustomManager(BaseManager):
 
 
 # task executed in a child process
-def adder_task(counter, lock):
+def adder_task(counter):
     for i in range(1000):
-        with lock:
-            counter.increment()
+        counter.increment()
 
 
 # task executed in a child process
-def subtractor_task(counter, lock):
+def subtractor_task(counter):
     for i in range(1000):
-        with lock:
-            counter.decrement()
+        counter.decrement()
 
 
 # protect the entry point
 if __name__ == "__main__":
     # register the custom class on the custom manager
     # so it knows how to make it
-    CustomManager.register("UnsafeCounter", UnsafeCounter)
-    lock = Lock()
+    CustomManager.register("SafeCounter", SafeCounter)
 
     # create manager
     with CustomManager() as manager:
         # create the counter on the manager server process
         # and return proxy obj that can be shared with the child
         # processes and used to interact with the hosted object
-        counter = manager.UnsafeCounter(0)
+        counter = manager.SafeCounter(0)
         # create child processes
-        adder_process = Process(target=adder_task,
-                                args=(counter, lock))
-        subtractor_process = Process(target=subtractor_task,
-                                     args=(counter, lock))
+        adder_process = Process(target=adder_task, args=(counter,))
+        subtractor_process = Process(target=subtractor_task, args=(counter,))
         # start child processes
         adder_process.start()
         subtractor_process.start()
